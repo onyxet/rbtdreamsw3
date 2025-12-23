ae35fccd7d3d94fbb623bed8c6664790c29ad04a
Rodion Sabodash <ros@matterlabs.dev>
2025-12-23 11:21:06 +0100

    fixes

--- dao-with-api-events/src/main.ts
+++ dao-with-api-events/src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS for frontend
   app.enableCors({
     origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'OPTIONS'],
